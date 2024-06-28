6cff50569eb894a8797e0cc15a20906a683a44c7
Andrew Shearer <andrewshearerdev@gmail.com>
2024-06-28 16:31:27 -0700

    updated PhpButton component to show data from php server once returned

--- vue/src/components/PhpButton.vue
+++ vue/src/components/PhpButton.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+type JsonResponse = {
+  message: string;
+};
+
+const stateData = ref<JsonResponse>({ message: "" });
+
 const phpButton = async () => {
   const response = await fetch("http://localhost:8000");
-  const data = await response.json();
-  console.log("data: ", data);
+  const data: JsonResponse = await response.json();
+
+  stateData.value = data;
 };
 </script>
 
 <template>
   <section class="section">
     <button @click="phpButton">PHP</button>
+
+    <!-- render the stateData if there is a value inside a pre tag -->
+    <pre v-if="stateData.message">{{ stateData.message }}</pre>
   </section>
 </template>
